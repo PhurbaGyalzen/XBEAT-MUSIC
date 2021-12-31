77ed58a818ae9c755b5f728aa00b73936d26cb9a
PhurbaGyalzen <sphurba861@gmail.com>
2021-12-31 19:30:08 +0545

    bottom naviagtion, splash screen and app icon added

---
+++ android/app/src/main/kotlin/com/example/xbeat/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.xbeat
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/pages/root_app.dart
@@ -0,0 +1,115 @@
+import 'package:flutter/material.dart';
+
+class RootApp extends StatefulWidget {
+  const RootApp({Key? key}) : super(key: key);
+
+  @override
+  _RootAppState createState() => _RootAppState();
+}
+
+class _RootAppState extends State<RootApp> {
+  int activeTab = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: getBody(),
+      bottomNavigationBar: getButtomAppBar(),
+    );
+  }
+
+  Widget getBody() {
+    return IndexedStack(
+      index: activeTab,
+      children: [
+        Center(
+          child: Text(
+            'Home',
+            style: TextStyle(
+              color: Colors.white,
+            ),
+          ),
+        ),
+        Center(
+          child: Text(
+            'Library',
+            style: TextStyle(
+              color: Colors.white,
+            ),
+          ),
+        ),
+        Center(
+          child: Text(
+            'Bookmark',
+            style: TextStyle(
+              color: Colors.white,
+            ),
+          ),
+        ),
+        Center(
+          child: Text(
+            'Settings',
+            style: TextStyle(
+              color: Colors.white,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget getButtomAppBar() {
+    return (BottomAppBar(
+      color: Colors.black,
+      child: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            IconButton(
+                onPressed: () {
+                  setState(() {
+                    activeTab = 0;
+                  });
+                },
+                icon: Icon(
+                  Icons.home,
+                  color: activeTab==0 ? Colors.blue[300]: Colors.white,
+                )),
+            IconButton(
+                onPressed: () {
+                  setState(() {
+                    activeTab = 1;
+                  });
+                },
+                icon: Icon(
+                  Icons.library_add_check,
+                  color: activeTab== 1 ? Colors.blue[300]: Colors.white,
+                )),
+            IconButton(
+                onPressed: () {
+                  setState(() {
+                    activeTab = 2;
+                  });
+                },
+                icon: Icon(
+                  Icons.bookmark,
+                  color: activeTab == 2 ? Colors.blue[300]: Colors.white,
+                )),
+            IconButton(
+              onPressed: () {
+                setState(() {
+                  activeTab = 3;
+                });
+              },
+              icon: Icon(
+                Icons.settings,
+                color: activeTab == 3 ? Colors.blue[300]: Colors.white,
+              ),
+            ),
+          ],
+        ),
+      ),
+    ));
+  }
+}
