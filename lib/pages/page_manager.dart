d682508c7ce2e30a1de64a588684d967806ce4e1
PhurbaGyalzen <sphurba861@gmail.com>
2022-02-07 17:45:30 +0545

    playlist audio player feature added

--- lib/pages/page_manager.dart
+++ lib/pages/page_manager.dart
@@ -1,33 +1,70 @@
 import 'package:flutter/material.dart';
 import 'package:just_audio/just_audio.dart';
 
+import '../notifiers/play_button_notifier.dart';
+import '../notifiers/progress_notifier.dart';
+import '../notifiers/repeat_button_notifier.dart';
+
 class PageManager {
-  static const url =
-      'http://192.168.1.17:3000/stream/song/Real Friends - CAMILA CABELLO ft SWAE LEE.mp3';
+  final currentSongTitleNotifier = ValueNotifier<String>('');
+  final playlistNotifier = ValueNotifier<List<String>>([]);
+  final progressNotifier = ProgressNotifier();
+  final repeatButtonNotifier = RepeatButtonNotifier();
+  final isFirstSongNotifier = ValueNotifier<bool>(true);
+  final playButtonNotifier = PlayButtonNotifier();
+  final isLastSongNotifier = ValueNotifier<bool>(true);
+  final isShuffleModeEnabledNotifier = ValueNotifier<bool>(false);
+  late ConcatenatingAudioSource _playlist;
+
   late AudioPlayer _audioPlayer;
+
   PageManager() {
     _init();
   }
+
   void _init() async {
     _audioPlayer = AudioPlayer();
-    await _audioPlayer.setUrl(url);
+    _setInitialPlaylist();
+    _listenForChangesInPlayerState();
+    _listenForChangesInPlayerPosition();
+    _listenForChangesInBufferedPosition();
+    _listenForChangesInTotalDuration();
+    _listenForChangesInSequenceState();
+  }
+
+  // TODO: set playlist
+  void _setInitialPlaylist() async {
+    const prefix = 'https://www.soundhelix.com/examples/mp3';
+    final song1 = Uri.parse('$prefix/SoundHelix-Song-1.mp3');
+    final song2 = Uri.parse('$prefix/SoundHelix-Song-2.mp3');
+    final song3 = Uri.parse('$prefix/SoundHelix-Song-3.mp3');
+    _playlist = ConcatenatingAudioSource(children: [
+      AudioSource.uri(song1, tag: 'Song 1'),
+      AudioSource.uri(song2, tag: 'Song 2'),
+      AudioSource.uri(song3, tag: 'Song 3'),
+    ]);
+    await _audioPlayer.setAudioSource(_playlist);
+  }
+
+  void _listenForChangesInPlayerState() {
     _audioPlayer.playerStateStream.listen((playerState) {
       final isPlaying = playerState.playing;
       final processingState = playerState.processingState;
       if (processingState == ProcessingState.loading ||
           processingState == ProcessingState.buffering) {
-        buttonNotifier.value = ButtonState.loading;
+        playButtonNotifier.value = ButtonState.loading;
       } else if (!isPlaying) {
-        buttonNotifier.value = ButtonState.paused;
+        playButtonNotifier.value = ButtonState.paused;
       } else if (processingState != ProcessingState.completed) {
-        buttonNotifier.value = ButtonState.playing;
+        playButtonNotifier.value = ButtonState.playing;
       } else {
-        // completed
         _audioPlayer.seek(Duration.zero);
         _audioPlayer.pause();
       }
     });
+  }
 
+  void _listenForChangesInPlayerPosition() {
     _audioPlayer.positionStream.listen((position) {
       final oldState = progressNotifier.value;
       progressNotifier.value = ProgressBarState(
@@ -36,6 +73,9 @@ class PageManager {
         total: oldState.total,
       );
     });
+  }
+
+  void _listenForChangesInBufferedPosition() {
     _audioPlayer.bufferedPositionStream.listen((bufferedPosition) {
       final oldState = progressNotifier.value;
       progressNotifier.value = ProgressBarState(
@@ -44,6 +84,9 @@ class PageManager {
         total: oldState.total,
       );
     });
+  }
+
+  void _listenForChangesInTotalDuration() {
     _audioPlayer.durationStream.listen((totalDuration) {
       final oldState = progressNotifier.value;
       progressNotifier.value = ProgressBarState(
@@ -54,7 +97,32 @@ class PageManager {
     });
   }
 
-  void play() {
+  void _listenForChangesInSequenceState() {
+    _audioPlayer.sequenceStateStream.listen((sequenceState) {
+      if (sequenceState == null) return;
+      // TODO: update current song title
+      final currentItem = sequenceState.currentSource;
+      final title = currentItem?.tag as String?;
+      currentSongTitleNotifier.value = title ?? '';
+      // TODO: update playlist
+      final playlist = sequenceState.effectiveSequence;
+      print(playlist);
+      final titles = playlist.map((item) => item.tag as String).toList();
+      playlistNotifier.value = titles;
+      // TODO: update shuffle mode
+      isShuffleModeEnabledNotifier.value = sequenceState.shuffleModeEnabled;
+      // TODO: update previous and next buttons
+      if (playlist.isEmpty || currentItem == null) {
+        isFirstSongNotifier.value = true;
+        isLastSongNotifier.value = true;
+      } else {
+        isFirstSongNotifier.value = playlist.first == currentItem;
+        isLastSongNotifier.value = playlist.last == currentItem;
+      }
+    });
+  }
+
+  void play() async {
     _audioPlayer.play();
   }
 
@@ -62,33 +130,54 @@ class PageManager {
     _audioPlayer.pause();
   }
 
-  void dispose() {
-    _audioPlayer.dispose();
-  }
-
   void seek(Duration position) {
     _audioPlayer.seek(position);
   }
 
-  final progressNotifier = ValueNotifier<ProgressBarState>(
-    ProgressBarState(
-      current: Duration.zero,
-      buffered: Duration.zero,
-      total: Duration.zero,
-    ),
-  );
-  final buttonNotifier = ValueNotifier<ButtonState>(ButtonState.paused);
-}
+  void dispose() {
+    _audioPlayer.dispose();
+  }
 
-class ProgressBarState {
-  ProgressBarState({
-    required this.current,
-    required this.buffered,
-    required this.total,
-  });
-  final Duration current;
-  final Duration buffered;
-  final Duration total;
-}
+  void onRepeatButtonPressed() {
+    repeatButtonNotifier.nextState();
+    switch (repeatButtonNotifier.value) {
+      case RepeatState.off:
+        _audioPlayer.setLoopMode(LoopMode.off);
+        break;
+      case RepeatState.repeatSong:
+        _audioPlayer.setLoopMode(LoopMode.one);
+        break;
+      case RepeatState.repeatPlaylist:
+        _audioPlayer.setLoopMode(LoopMode.all);
+    }
+  }
 
-enum ButtonState { paused, playing, loading }
+  void onPreviousSongButtonPressed() {
+    _audioPlayer.seekToPrevious();
+  }
+
+  void onNextSongButtonPressed() {
+    _audioPlayer.seekToNext();
+  }
+
+  void onShuffleButtonPressed() async {
+    final enable = !_audioPlayer.shuffleModeEnabled;
+    if (enable) {
+      await _audioPlayer.shuffle();
+    }
+    await _audioPlayer.setShuffleModeEnabled(enable);
+  }
+
+  void addSong() {
+    final songNumber = _playlist.length + 1;
+    const prefix = 'https://www.soundhelix.com/examples/mp3';
+    final song = Uri.parse('$prefix/SoundHelix-Song-$songNumber.mp3');
+    _playlist.add(AudioSource.uri(song, tag: 'Song $songNumber'));
+  }
+
+  void removeSong() {
+    final index = _playlist.length - 1;
+    if (index < 0) return;
+    _playlist.removeAt(index);
+  }
+}
--- lib/pages/steaming.dart
+++ lib/pages/steaming.dart
@@ -1,6 +1,9 @@
 import 'package:audio_video_progress_bar/audio_video_progress_bar.dart';
 import 'page_manager.dart';
 import 'package:flutter/material.dart';
+import '../notifiers/play_button_notifier.dart';
+import '../notifiers/progress_notifier.dart';
+import '../notifiers/repeat_button_notifier.dart';
 
 class StreamApp extends StatefulWidget {
   const StreamApp({Key? key}) : super(key: key);
@@ -9,9 +12,10 @@ class StreamApp extends StatefulWidget {
   _StreamAppState createState() => _StreamAppState();
 }
 
-class _StreamAppState extends State<StreamApp> {
-  late final PageManager _pageManager;
+// use GetIt or Provider rather than a global variable in a real project
+late final PageManager _pageManager;
 
+class _StreamAppState extends State<StreamApp> {
   @override
   void initState() {
     super.initState();
@@ -32,56 +36,239 @@ class _StreamAppState extends State<StreamApp> {
         padding: const EdgeInsets.all(20.0),
         child: Column(
           children: [
-            const Spacer(),
-            ValueListenableBuilder<ProgressBarState>(
-              valueListenable: _pageManager.progressNotifier,
-              builder: (_, value, __) {
-                return ProgressBar(
-                  progress: value.current,
-                  buffered: value.buffered,
-                  total: value.total,
-                  onSeek: _pageManager.seek,
-                  timeLabelTextStyle: TextStyle(color: Colors.white),
-                  timeLabelLocation: TimeLabelLocation.below,
-                  timeLabelPadding: 5,
-                );
-              },
-            ),
-            ValueListenableBuilder<ButtonState>(
-              valueListenable: _pageManager.buttonNotifier,
-              builder: (_, value, __) {
-                switch (value) {
-                  case ButtonState.loading:
-                    return Container(
-                      margin: const EdgeInsets.all(8.0),
-                      width: 32.0,
-                      color: Colors.black,
-                      height: 32.0,
-                      child: const CircularProgressIndicator(),
-                    );
-                  case ButtonState.paused:
-                    return IconButton(
-                      icon: const Icon(Icons.play_arrow),
-                      iconSize: 32.0,
-                      color: Colors.white,
-                      onPressed: _pageManager.play,
-                    );
-                  case ButtonState.playing:
-                    return IconButton(
-                      icon: const Icon(Icons.pause),
-                      iconSize: 32.0,
-                      color: Colors.white,
-                      onPressed: _pageManager.pause,
-                    );
-                }
-              },
-            ),
-            SizedBox(
-              height: 30,
-            )
+            CurrentSongTitle(),
+            Playlist(),
+            AddRemoveSongButtons(),
+            AudioProgressBar(),
+            AudioControlButtons(),
           ],
         ),
       ),
+    );
+  }
+}
+
+class CurrentSongTitle extends StatelessWidget {
+  const CurrentSongTitle({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return ValueListenableBuilder<String>(
+      valueListenable: _pageManager.currentSongTitleNotifier,
+      builder: (_, title, __) {
+        return Padding(
+          padding: const EdgeInsets.only(top: 8.0),
+          child: Text(title,
+              style: TextStyle(
+                fontSize: 40,
+                color: Colors.white,
+              )),
+        );
+      },
+    );
+  }
+}
+
+class Playlist extends StatelessWidget {
+  const Playlist({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: ValueListenableBuilder<List<String>>(
+        valueListenable: _pageManager.playlistNotifier,
+        builder: (context, playlistTitles, _) {
+          return ListView.builder(
+            itemCount: playlistTitles.length,
+            itemBuilder: (context, index) {
+              return ListTile(
+                title: Text(
+                  '${playlistTitles[index]}',
+                  style: TextStyle(color: Colors.white),
+                ),
+              );
+            },
+          );
+        },
+      ),
+    );
+  }
+}
+
+class AddRemoveSongButtons extends StatelessWidget {
+  const AddRemoveSongButtons({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 20.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          FloatingActionButton(
+            onPressed: _pageManager.addSong,
+            child: Icon(Icons.add),
+          ),
+          FloatingActionButton(
+            onPressed: _pageManager.removeSong,
+            child: Icon(Icons.remove),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class AudioProgressBar extends StatelessWidget {
+  const AudioProgressBar({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return ValueListenableBuilder<ProgressBarState>(
+      valueListenable: _pageManager.progressNotifier,
+      builder: (_, value, __) {
+        return ProgressBar(
+          progress: value.current,
+          buffered: value.buffered,
+          total: value.total,
+          onSeek: _pageManager.seek,
+          timeLabelTextStyle: TextStyle(color: Colors.grey[400]),
+        );
+      },
+    );
+  }
+}
+
+class AudioControlButtons extends StatelessWidget {
+  const AudioControlButtons({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 60,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          RepeatButton(),
+          PreviousSongButton(),
+          PlayButton(),
+          NextSongButton(),
+          ShuffleButton(),
+        ],
+      ),
+    );
+  }
+}
+
+class RepeatButton extends StatelessWidget {
+  const RepeatButton({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return ValueListenableBuilder<RepeatState>(
+      valueListenable: _pageManager.repeatButtonNotifier,
+      builder: (context, value, child) {
+        Icon icon;
+        switch (value) {
+          case RepeatState.off:
+            icon = Icon(Icons.repeat, color: Colors.grey);
+            break;
+          case RepeatState.repeatSong:
+            icon = Icon(Icons.repeat_one, color: Colors.blue);
+            break;
+          case RepeatState.repeatPlaylist:
+            icon = Icon(Icons.repeat, color: Colors.blue);
+            break;
+        }
+        return IconButton(
+          icon: icon,
+          onPressed: _pageManager.onRepeatButtonPressed,
+        );
+      },
+    );
+  }
+}
+
+class PreviousSongButton extends StatelessWidget {
+  const PreviousSongButton({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return ValueListenableBuilder<bool>(
+      valueListenable: _pageManager.isFirstSongNotifier,
+      builder: (_, isFirst, __) {
+        return IconButton(
+          icon: Icon(Icons.skip_previous, color: isFirst? Colors.grey: Colors.white),
+          onPressed:
+              (isFirst) ? null : _pageManager.onPreviousSongButtonPressed,
+        );
+      },
+    );
+  }
+}
+
+class PlayButton extends StatelessWidget {
+  const PlayButton({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return ValueListenableBuilder<ButtonState>(
+      valueListenable: _pageManager.playButtonNotifier,
+      builder: (_, value, __) {
+        switch (value) {
+          case ButtonState.loading:
+            return Container(
+              margin: EdgeInsets.all(8.0),
+              width: 32.0,
+              height: 32.0,
+              child: CircularProgressIndicator(),
+            );
+          case ButtonState.paused:
+            return IconButton(
+              icon: Icon(Icons.play_arrow, color: Colors.white),
+              iconSize: 32.0,
+              onPressed: _pageManager.play,
+            );
+          case ButtonState.playing:
+            return IconButton(
+              icon: Icon(Icons.pause, color: Colors.white),
+              iconSize: 32.0,
+              onPressed: _pageManager.pause,
+            );
+        }
+      },
+    );
+  }
+}
+
+class NextSongButton extends StatelessWidget {
+  const NextSongButton({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return ValueListenableBuilder<bool>(
+      valueListenable: _pageManager.isLastSongNotifier,
+      builder: (_, isLast, __) {
+        return IconButton(
+          icon: Icon(
+            Icons.skip_next,
+            color: isLast ? Colors.grey : Colors.white,
+          ),
+          onPressed: (isLast) ? null : _pageManager.onNextSongButtonPressed,
+        );
+      },
+    );
+  }
+}
+
+class ShuffleButton extends StatelessWidget {
+  const ShuffleButton({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return ValueListenableBuilder<bool>(
+      valueListenable: _pageManager.isShuffleModeEnabledNotifier,
+      builder: (context, isEnabled, child) {
+        return IconButton(
+          icon: (isEnabled)
+              ? Icon(
+                  Icons.shuffle,
+                  color: Colors.blue,
+                )
+              : Icon(Icons.shuffle, color: Colors.grey),
+          onPressed: _pageManager.onShuffleButtonPressed,
+        );
+      },
     );
   }
 }
