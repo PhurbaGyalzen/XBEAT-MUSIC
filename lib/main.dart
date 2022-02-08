ae056119e9c71f6f1853617927c55bad8b33d50c
PhurbaGyalzen <sphurba861@gmail.com>
2022-02-08 10:20:36 +0545

    landscape mode disabled

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:xbeat/pages/root_app.dart';
+import 'package:flutter/services.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations(
+      [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
     home: RootApp(),
